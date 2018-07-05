2c2af7ee94c8cce30eca5615afdede1d77e43f45
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2018-07-05 22:19:07 +0100

    biult func to make deck

--- deck.go
+++ deck.go
@@ -8,11 +8,42 @@ import (
 
 type deck []string
 
-//reciver ?
-func (cards deck) print() {
+// this is a proper deck
+func newDeck() deck {
 
-	for i, card := range cards {
+	cards := deck{}
+
+	suits := []string{" of Hearts", " of Dimonds", " of Clubs", " of Spades"}
+	cardNo := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "nine",
+		"Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range suits {
+		for _, card := range cardNo {
+
+			fmt.Println(card + suit)
+
+		}
+		fmt.Println()
+	}
+
+	return cards
+}
+
+//reciver Allows the used of a func by any varible of type
+//i.e in this case any var of typpe deck will have access to these funcs
+//reciver is a var that is passed before the func name annotation
+func (d deck) print() {
+
+	for i, card := range d {
 
 		fmt.Println(i, card)
 	}
+}
+
+func (d deck) shuffle() {
+	fmt.Println("shuffle", d)
+}
+
+func (d deck) deal() {
+
 }
