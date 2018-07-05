ea46674894ab03a8e443618309463f33dbb2063e
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2018-07-05 21:24:15 +0100

    removed loops from main

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
 
 	cards := deck{"Ace of Spades", newCard()}
@@ -11,17 +7,7 @@ func main() {
 	cards = append(cards, "Ace of Clubs")
 	cards = append(cards, "Ace of Dimonds")
 
-	//difrent ways of defining a for loop in go
-	//type one
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	//type two
-	for i := 0; i < len(cards); i++ {
-
-		fmt.Println(cards[i])
-	}
+	cards.print()
 }
 
 func newCard() string {
