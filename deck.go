1a3b8724a8d82ecd47778ae301f00e737a8e3ef0
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2018-07-05 21:23:29 +0100

    added print to deck

--- deck.go
+++ deck.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 // creates new type of deck
 
 type deck []string
+
+//reciver ?
+func (cards deck) print() {
+
+	for i, card := range cards {
+
+		fmt.Println(i, card)
+	}
+}
