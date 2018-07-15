74577ef9b9b6ca2bd8a950f17df195c32371d173
Jason Sebastian Flynn <J.S.Flynn@outlook.com>
2018-07-15 17:36:11 +0100

    added test for deal()

--- deck_test.go
+++ deck_test.go
@@ -20,3 +20,21 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected Ace of hearts , got %v", d[len(d)-1])
 	}
 }
+
+func TestDeal(t *testing.T) {
+
+	d := newDeck()
+	hSize := 5
+
+	hand, stack := deal(d, hSize)
+
+	if len(hand) != 5 {
+
+		t.Errorf("expeted %v, got %v ", hSize, len(hand))
+	}
+
+	if len(stack) != (len(d) - hSize) {
+
+		t.Errorf("ecpected %v, got %v", (len(d) - hSize), len(stack))
+	}
+}
